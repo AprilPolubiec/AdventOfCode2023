8b3859d454ee6635dd682806b4d78967f54c4558
April Polubiec <aprilpolubiec@gmail.com>
2023-12-09 22:13:16 +0000

    part 2 (messy)

--- day3/main.py
+++ day3/main.py
@@ -2,13 +2,14 @@
 import os
 
 def read_file(filename: str):
-    print(os.path.dirname(os.path.realpath(__file__)))
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename), "r") as f:
         lines = f.readlines()
     return lines
 
 def is_symbol(c: str):
     return c is not None and not c.isdigit() and not c == "."
+def is_asterisk(c: str):
+    return c == "*"
 
 def get_neighbors(all_neighbors: [[]], current_pos: ()):
     row_idx = current_pos[0]
@@ -29,32 +30,70 @@ def get_neighbors(all_neighbors: [[]], current_pos: ()):
 
     return (left, right, top, bottom, top_left, top_right, bottom_left, bottom_right)
 
-def get_parts(in_str: list[str]):
+# Given a string, returns as an array where each digit is stored in full in each index it occupies
+def create_matrix(in_str: list[str]):
     char_matrix = [ [c for c in l] for l in in_str ]
-    parts = []
+
+    current_num_start = None
+    current_num = ""
+
     for row_idx in range(len(char_matrix)):
         row = char_matrix[row_idx]
-        curr_digit = ""
-        found_neighbor = False
         for char_idx in range(len(row)):
             char = row[char_idx]
-            if not char.isdigit() and curr_digit != "":
-                if found_neighbor:
-                    parts.append(curr_digit)
-                    found_neighbor = False
-                curr_digit = ""
-                continue
+            if not char.isdigit(): # Clear the number we are tracking
+                current_num_start = None
+                current_num = ""
             if char.isdigit():
-                curr_digit += char
+                if current_num_start != None:
+                    # We are looking at a number
+                    current_num += char # update our current number
+                    for i in range(current_num_start, char_idx + 1): # backfill all indexes
+                        char_matrix[row_idx][i] = current_num
+                else:
+                    current_num_start = char_idx
+                    char_matrix[row_idx][char_idx] = char
+                    current_num = char
+                continue
+
+    return char_matrix
+    
+def get_parts(in_str: list[str]):
+    char_matrix = create_matrix(in_str)
+    parts = []
+    gear_ratios = []
+
+    row_idx = 0
+    while row_idx < len(char_matrix):
+        is_checking_digit = False
+        row = char_matrix[row_idx]
+        char_idx = 0
+        while char_idx < len(row):
+            char = row[char_idx]
+            if char.isdigit():
                 neighbors = get_neighbors(char_matrix, (row_idx, char_idx))
                 # Look for neighbors that are symbols
-                for neighbor in neighbors:
-                    if is_symbol(neighbor):
-                        found_neighbor = True
-                        continue
-        if curr_digit != "" and found_neighbor:
-            parts.append(curr_digit)
-    return parts
+                symbols_neighbors = filter(lambda x:is_symbol(x),neighbors)
+                if len(list(symbols_neighbors)) >= 1:
+                    if len(parts) == 0 or parts[-1] != char or (parts[-1] == char and is_checking_digit == False):
+                        # We've already seen this digit do nothing
+                        parts.append(char)                    
+                else:
+                    pass
+                    # print("no neighbors: ", char, row_idx)
+                is_checking_digit = True
+            else:
+                if is_asterisk(char):
+                    neighbors = get_neighbors(char_matrix, (row_idx, char_idx))
+                    neighbors = set(list(filter(lambda x:x.isdigit(),neighbors)))
+                    if len(neighbors) == 2:
+                        print(neighbors)
+                        gear_ratios.append(int(list(neighbors)[0])*int(list(neighbors)[1]))
+                is_checking_digit = False
+            char_idx += 1
+        row_idx += 1
+    # print(gear_ratios)
+    return parts, gear_ratios
 
 def get_sum(parts):
     sum = 0
@@ -62,9 +101,13 @@ def get_sum(parts):
         sum += int(i)
     return sum
 
+def get_gear_ratios_sum(file_content):
+    sum = 0
+    return sum
+
 if __name__ == '__main__':
     file_content = read_file("input.txt")
-    res = get_parts(file_content)
-    # print(res)
-    sum = get_sum(res)
-    print(sum)
+    parts, gear_ratios = get_parts(file_content)
+    sum = get_sum(parts)
+    gear_sum = get_sum(gear_ratios)
+    print(gear_sum)
--- day3/test.py
+++ day3/test.py
@@ -1,104 +1,141 @@
 import unittest
-from main import get_parts
+import os
+from main import create_matrix, get_parts, get_sum
 
 class TestSum(unittest.TestCase):
+    def test_create_matrix(self):
+       in_str = "...$...\n...342.\n......."
+       # Expect false
+       c_matrix = create_matrix(in_str.split("\n"))
+
+       self.assertListEqual(c_matrix[0], [".", ".", ".", "$", ".", ".", "."])
+       self.assertListEqual(c_matrix[1], [".", ".", ".", "342", "342", "342", "."])
+       self.assertListEqual(c_matrix[2], [".", ".", ".", ".", ".", ".", "."])
+    
     def test_digit_next_to_no_symbols_is_not_part(self):
        in_str = "...353."
        # Expect false
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(len(res), 0)
 
     def test_digit_vertical_to_no_symbols_is_not_part(self):
        in_str = ".......\n..213..\n......."
        # Expect false
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(len(res), 0)
 
     def test_single_digit_next_to_symbol_is_part(self):
        in_str = "...3#..\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["3"])
        in_str = "..#3...\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["3"])
 
     def test_single_digit_vertical_to_symbol_is_part(self):
        in_str = ".......\n...3...\n...$..."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["3"])
        in_str = "...$...\n...3...\n......."
         # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["3"])
 
     def test_single_digit_diagonal_to_symbol_is_part(self):
        in_str = "...+...\n..4....\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["4"])
        in_str = ".......\n..4....\n...+..."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["4"])
 
     def test_multiple_digits_next_to_symbol_is_part(self):
        in_str = "..34#..\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["34"])
        in_str = "..#36..\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["36"])
 
     def test_multiple_digits_vertical_to_symbol_is_part(self):
        # Symbol below first digit
        in_str = ".......\n...32..\n...$..."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["32"])
 
        # Symbol above first digit 
        in_str = "...$...\n...342.\n......."
         # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["342"])
 
         # Symbol above second digit 
        in_str = "....$..\n...3223\n......."
         # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["3223"])
        # Symbol below third digit
        in_str = ".......\n..132..\n....$.."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["132"])
 
+    def test_example(self):
+       ex = ["467..114..",
+         "...*......",
+         "..35..633.",
+         "......#...",
+         "617*......",
+         ".....+.58.",
+         "..592.....",
+         "......755.",
+         "...$.*....",
+         ".664.598.."]
+       (res, gears) = get_parts(ex)
+       print(res)
+       self.assertEqual(get_sum(res), 467835)
     def test_multiple_digits_diagonal_to_symbol_is_part(self):
        # Symbol diagonal top right
        in_str = ".....%.\n.8390..\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["8390"])
        # Symbol diagonal top left
        in_str = "..@....\n...39..\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["39"])
        # Symbol diagonal bottom right
        in_str = ".......\n.8273..\n.....(."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["8273"])
        # Symbol diagonal bottom left
        in_str = "...!...\n....302\n......."
        # Expect true
-       res = get_parts(in_str.split("\n"))
+       (res, gears) = get_parts(in_str.split("\n"))
        self.assertEqual(res, ["302"])
+   
+    def test_get_parts_on_input_file(self):
+      with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt"), "r") as f:
+        lines = f.readlines()
+      sum = get_parts(lines[:2])
+      self.assertEqual(sum, ["411", "363", "134", "463", "775", "506"])
+
+    def test_input_file(self):
+      with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt"), "r") as f:
+        lines = f.readlines()
+      parts, gears = get_parts(lines)
+      sum = get_sum(parts)
+      self.assertEqual(sum, 536202)
 
 if __name__ == '__main__':
     unittest.main()
